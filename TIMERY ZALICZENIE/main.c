524cbcb0dcab6d0c3fb4be25d383cc1d0677c848
unknown <janekrutkows@gmail.com>
2025-05-21 14:44:44 +0200

    ok

--- TIMERY ZALICZENIE/main.c
+++ TIMERY ZALICZENIE/main.c
@@ -21,60 +21,11 @@ for(iLoopCounter=0; iLoopCounter< iDelayInMS; iLoopCounter++){}
 int main(){
 
         LedInit();
-        KeyboardInit();
+				InitTimer0Match0(10000);
         while(1){
-            switch (eLedState){
-                case LEDSTOP:
-                    if (eKeyboardRead()==BUTTON_0){
-                        eLedState=LEDLEFT;
-
-                    }
-                    else if(eKeyboardRead()==BUTTON_2){
-                        eLedState=LEDRIGHT;
-                    }
-                    else{
-                        eLedState=LEDSTOP;
-                    }
-                    break;
-
-                case LEDLEFT:
-
-                    if (eKeyboardRead()==BUTTON_1){
-                        eLedState=LEDSTOP;
-                    }
-                    else if(eKeyboardRead()==BUTTON_3){
-                        eLedState=LEDBLINK;
-                    }
-                    else {
-                        LedStepLeft();
-                        eLedState=LEDLEFT;
-                    }
-                    break;
-
-                case LEDRIGHT:
-
-                    if (eKeyboardRead()==BUTTON_1){
-                        eLedState=LEDSTOP;
-                    }
-                    else {
-                        LedStepRight();
-                        eLedState=LEDRIGHT;
-                    }
-                    break;
-
-                 case LEDBLINK:
-                    if (ucBlinkcounter==3){
-                      eLedState=LEDRIGHT;
-                      ucBlinkcounter=0;
-                     }
-
-										else{
-											LedOn(4);
-											LedOn(1);
-											ucBlinkcounter++;
-										}
-										break;
-              }
-        Delay(100);
-    }
+					WaitOnTimer0Match0();
+					LedStepLeft();
+					
+					
+				}
 }
--- TIMERY ZALICZENIE/timer.c
+++ TIMERY ZALICZENIE/timer.c
@@ -1,35 +1,34 @@
 #include <LPC21xx.H>
 #include "timer.h"
-#define Timer0_ON_bm (1<<0)
-#define Timer0_RESET_bm (1<<1)
-#define Timer_interrupt_bm (1<<0)
-#define Timer_Reset_mr_bm (1<<1)
-#define Timer_Flag_bm (1<<0)
+
+#define TIMER_ON_bm (1<<0)
+#define TIMER_RESET_bm (1<<1)
+#define TIMER_INTERRUPT_bm (1<<0)
+#define TIMER_MR_RESET_bm (1<<1)
+#define TIMER_FLAG_bm (1<<0)
 
 void InitTimer0(){
-	T0TCR = Timer0_ON_bm;
+	T0TCR = TIMER_ON_bm; // Wlaczanie timera
 }
 
 void InitTimer0Match0(unsigned int iDelayTime){
-	T0MR0=(iDelayTime*15);
-	T0MCR= (Timer_interrupt_bm|Timer_Reset_mr_bm);
-	T0TCR |= Timer0_RESET_bm;
-	T0TCR &= (~Timer0_RESET_bm);
-	InitTimer0();
+	T0MR0 = (iDelayTime * 15); // Ustawia czas, po którym timer resetuje
+	T0MCR = (TIMER_INTERRUPT_bm | TIMER_MR_RESET_bm); // Ustawia mozliwosc przerwania i resetu
+	T0TCR |= TIMER_RESET_bm; // Resetuje timer
+	T0TCR &= (~TIMER_RESET_bm); // Wylacza reset
+	
+	InitTimer0(); // Wlacza timer
 }
 
-
 void WaitOnTimer0(unsigned int uiTime){
-	T0TCR |= Timer0_RESET_bm;
-	T0TCR = T0TCR&(~Timer0_RESET_bm);
-	while (T0TC<=(uiTime*15)){}
+	T0TCR |= TIMER_RESET_bm; 
+	T0TCR &= (~TIMER_RESET_bm); 
+	while (T0TC <= (uiTime * 15)){} // Czeka az licznik timera osiagnie wartosc
 }
 
 void WaitOnTimer0Match0(){
-	while ((T0IR&Timer_Flag_bm)==0){}
-	T0IR=Timer_Flag_bm;		
+	while ((T0IR & TIMER_FLAG_bm) == 0){} // Czeka, az pojawi sie flaga przerwania
+	T0IR = TIMER_FLAG_bm;	// Resetuje flage
 }
-
-
 
 
