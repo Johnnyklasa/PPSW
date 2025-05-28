d3ce5b9db827c936388188dd76fb12fe303a11a0
Johnnyklasa <janekrutkows@gmail.com>
2025-05-28 03:02:21 +0200

    ok

--- TOKENY/main1.c
+++ TOKENY/main1.c
@@ -20,6 +20,41 @@ enum CompResult eCompareString(char pcStr1[], char pcStr2[])
     
     return EQUAL;
 }
+enum Result {OK, ERROR};
+
+enum Result eHexStringToUInt(char pcStr[],unsigned int *puiValue)
+{
+    unsigned char ucCharacterCounter;
+    unsigned char ucCurrentCharacter;
+
+    *puiValue = 0;
+    if((pcStr[0] != '0') || (pcStr[1] != 'x') || (pcStr[2] == NULL))
+    {
+        return ERROR;
+    }
+    for(ucCharacterCounter = 2; pcStr[ucCharacterCounter] !=NULL ; ucCharacterCounter++)
+    {
+        ucCurrentCharacter = pcStr[ucCharacterCounter];
+        if (ucCharacterCounter == 6)
+        {
+            return ERROR;
+        }
+	  *puiValue = *puiValue << 4;
+        if((ucCurrentCharacter <= '9') && (ucCurrentCharacter >= '0'))
+        {
+            *puiValue = (*puiValue) | (ucCurrentCharacter - '0');
+        }
+        else if((ucCurrentCharacter <= 'F') && (ucCurrentCharacter >= 'A'))
+        {
+            *puiValue = (*puiValue) | (ucCurrentCharacter - 'A' + 10);
+        }
+        else
+        {
+            return ERROR;
+        }
+    }
+    return OK;
+}
 
 enum TokenType {STRING, NUMBER, KEYWORD}; 
 enum TokenCode {LD, ST, RST};
@@ -71,7 +106,7 @@ unsigned char ucFindTokensInString (char *pcString){
 			else if (ucCurrChar!=' '){
 				eState=TOKEN;
 				
-				asToken[ucCharCounter].uValue.pcString = pcString + ucCharCounter;// na dany element tablicy struktur na jego wartosc pointera na pc string w unii Value przypisujemy wskaznik na pierwszy znak kazdego stringa
+				asToken[ucTokenNumber].uValue.pcString = pcString + ucCharCounter;// na dany element tablicy struktur na jego wartosc pointera na pc string w unii Value przypisujemy wskaznik na pierwszy znak kazdego stringa
 				ucTokenNumber++;
 			}
 			else{
@@ -96,26 +131,56 @@ unsigned char ucFindTokensInString (char *pcString){
 		}
 	}	
 }	
-//Funkcja wyodrebnia max3 tokeny ze stringa oraz przypisuje pointer do pierwszego zanku kazdego z nich do danego elementu w tablicy struktur
-//Deklarujemy zmienna np pcStr="load 0X20 nigga"
-//Uzywamy funkcje ---(pcString) czyli dajemy adres Stringa na który ma wskazywac 
-// po rozdzeleniu do 3 osobnych struktur w kazdej z nich znajduje sie pointer do1 z 3 stringów wyodrebnonych przez funkcje
 
-enum Result {ALLRIGHT, ERROR};
 
-enum Result eSringToKeyword (char pcStr[],enum TokenCode *peKeywordCode){
+enum Results {ALLRIGHT, ERRORs};
+
+enum Results eStringToKeyword (char pcStr[],enum TokenCode *peKeywordCode){
 		unsigned char ucCharCounter;
-	  for (ucCharCounter=0; ucCharCounter<=MaxKeywordNumber; ucCharCounter++){
+	  for (ucCharCounter=0; ucCharCounter<MaxKeywordNumber; ucCharCounter++){
 			if(eCompareString(asKeywordList[ucCharCounter].cString,pcStr)==EQUAL){
 				*peKeywordCode= asKeywordList[ucCharCounter].eKeyword;
 				return ALLRIGHT;				
 			}
 		}
-return ERROR;
+return ERRORs;
 }
 
+void DecodeTokens(){
+		
+	unsigned char ucTokenCounter;
+	struct Token *psCurrenttoken; 
+	for (ucTokenCounter=0; ucTokenNumber>ucTokenCounter;ucTokenCounter++){
+		psCurrenttoken=&asToken[ucTokenCounter];
+		if (eStringToKeyword(psCurrenttoken->uValue.pcString, &psCurrenttoken->uValue.eKeyword)==ALLRIGHT){
+		//wez adres skladowej uiNumber znajdujacej sie w uValue, która z kolei jest skladowa struktury/unii wskazywanej przez psCurrenttoken".
+			psCurrenttoken->eType= KEYWORD;
+		}
+		else if (eHexStringToUInt(psCurrenttoken->uValue.pcString,&psCurrenttoken->uValue.uiNumber)==OK){
+			psCurrenttoken->eType= NUMBER;
+		}
+		else {
+			psCurrenttoken->eType= STRING;
+		}
+			
+	}
+}
+
+
+
+
+
+
+
+void DecodeMsg(char *pcString){
+	ucTokenNumber= ucFindTokensInString(pcString);
+	ReplaceCharactersInString(pcString,' ', NULL);
+	DecodeTokens();
 	
 	
+	
+	
+}
 
 
 
@@ -125,9 +190,11 @@ return ERROR;
 
 	
 int main(){
-	char pcStr[19]="Janek jest chujem";
-	
+	char pcStr[19]="store chuj jasiek";
+	enum TokenCode eEnum;
 	ucFindTokensInString(pcStr);
+	eStringToKeyword(pcStr,&eEnum);
+	
 	
 }
 
